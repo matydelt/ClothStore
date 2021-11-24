5abce2ecb9e06af6e677a7838db382bfae99ff23
matydelt <matymanda@hotmail.com>
2021-11-24 15:20:53 -0300

    post publications update

--- api/src/controllers/publicationControlles.ts
+++ api/src/controllers/publicationControlles.ts
@@ -9,7 +9,10 @@ export default class PublicationController {
             const { name, images, id, stock, mark, detail, price } = req.body
             const publication: Publication = new PublicationSchema({ name, images, stock, mark, detail, price });
             await publication.save();
-            await UserSchema.findById(id)
+            const user = await UserSchema.findById(id)
+            user?.publications.push(publication._id)
+            await user?.save();
+            console.log(!!user)
             res.sendStatus(200);
         } catch (e) {
             console.log(e)
@@ -17,16 +20,13 @@ export default class PublicationController {
         }
 
     }
-    // static async getUser(req: Request, res: Response): Promise<void> {
-    //     try {
-    //         const { email, password } = req.query
-    //         const user = await UserSchema.findOne({ email:email })
-    //         if (user && user.password === password)
-    //             res.json(user);
-    //         else res.send("usuario o contraseña erronea")
-    //     } catch (e) {
-    //         console.log(e)
-    //         res.sendStatus(500)
-    //     }
-    // }
+    static async getPublications(req: Request, res: Response): Promise<void> {
+        try {
+            const publications: Publication[] = await PublicationSchema.find()
+            res.json(publications);
+        } catch (e) {
+            console.log(e)
+            res.sendStatus(500)
+        }
+    }
 }
