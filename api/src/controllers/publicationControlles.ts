84ed7ff0e9c273b4c72555e2d741810303e5d312
matydelt <matymanda@hotmail.com>
2021-11-24 18:06:57 -0300

    post publications finished

--- api/src/controllers/publicationControlles.ts
+++ api/src/controllers/publicationControlles.ts
@@ -6,13 +6,13 @@ export default class PublicationController {
 
     static async setPublication(req: Request, res: Response) {
         try {
-            const { name, images, id, stock, mark, detail, price } = req.body
-            const publication: Publication = new PublicationSchema({ name, images, stock, mark, detail, price });
+            const { name, images, id, stock, mark, detail, price, categorie, gender } = req.body
+            const publication: Publication = new PublicationSchema({ name, images, stock, mark, detail, price, categorie, gender, author: id });
             await publication.save();
             const user = await UserSchema.findById(id)
-            user?.publications.push(publication._id)
+
+            user?.publications.push(publication)
             await user?.save();
-            console.log(!!user)
             res.sendStatus(200);
         } catch (e) {
             console.log(e)
@@ -24,6 +24,17 @@ export default class PublicationController {
         try {
             const publications: Publication[] = await PublicationSchema.find()
             res.json(publications);
+        } catch (e) {
+            console.log(e)
+            res.sendStatus(500)
+        }
+    }
+
+    static async putStock(req: Request, res: Response): Promise<void> {
+        try {
+            const { id, stock } = req.body
+            await PublicationSchema.findById(id).updateOne({ stock: stock })
+            res.send("stock modificado");
         } catch (e) {
             console.log(e)
             res.sendStatus(500)
--- api/src/routes/publications.ts
+++ api/src/routes/publications.ts
@@ -4,5 +4,5 @@ const router = express.Router();
 
 router.get("/publications", PublicationController.getPublications)
 router.post("/publications/new", PublicationController.setPublication)
-
+router.put("/publications/stock", PublicationController.putStock)
 module.exports = router;
