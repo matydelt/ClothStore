c50a03cff0faf692f303f5d316379f2ee75a99fe
lautaromugas8 <lautaro_mugas@hotmail.com>
2021-11-23 23:04:41 -0300

    implementacion de Auth0, botones para login, logout y signup

---
+++ client/src/components/AuthenticationButton.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginButton from "./LoginButton";
+import LogoutButton from "./LogoutButton";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const AuthenticationButton = (): JSX.Element => {
+  const { isAuthenticated } = useAuth0();
+
+  return isAuthenticated ? <LogoutButton /> : <LoginButton />;
+};
+
+export default AuthenticationButton;
--- client/src/react-app-env.d.ts
+++ client/src/react-app-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="react-scripts" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: "development" | "production" | "test";
+    PUBLIC_URL: string;
+    REACT_APP_AUTH0_DOMAIN: string;
+    REACT_APP_AUTH0_CLIENT_ID: string;
+  }
+}
