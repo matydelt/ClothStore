91e5e25fc24d6546701c4779bcbdcde7d37006ff
Christian Nordfors <christiannordfors@gmail.com>
2021-11-24 22:18:13 -0300

    create publication form

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,7 +2,7 @@ import express, { Application, Request, Response, NextFunction } from 'express';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
-const { users, publications } = require("./routes/index")
+const { users, publications, cloudinary } = require("./routes/index")
 require('./db.ts'); // conexion a base de datos 
 
 const server: Application = express();
@@ -23,7 +23,7 @@ server.use((req: Request, res: Response, next: NextFunction) => {
   next();
 });
 
-server.use('/', [users, publications]);
+server.use('/', [users, publications, cloudinary]);
 // server.use('/', publications);
 
 // Error catching endware.
--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -2,10 +2,12 @@ import express from "express"
 const router = express.Router();
 const users = require("./users.ts")
 const publications = require("./publications")
+const cloudinary = require("./cloudinary")
 
 
 module.exports = {
     router,
     users,
-    publications
+    publications,
+    cloudinary
 };
