cc5123972cc30728ebf93aec57ee39f7f797d3a3
lautaromugas8 <lautaro_mugas@hotmail.com>
2021-11-24 23:16:30 -0300

    formulario de registro

--- client/src/components/LoginButton.tsx
+++ client/src/components/LoginButton.tsx
@@ -4,11 +4,16 @@ import { useAuth } from "../hooks/useAuth";
 interface Props {
   email: string;
   password: string;
+  google?: boolean;
 }
 
 const LoginButton = (props: Props): JSX.Element => {
-  const { signin } = useAuth();
-  return <button onClick={() => signin(props)}>Log In</button>;
+  const { signin, googleSignin } = useAuth();
+  if (props.google) {
+    return <button onClick={googleSignin}>Log in with Google</button>;
+  } else {
+    return <button onClick={() => signin(props)}>Log In</button>;
+  }
 };
 
 export default LoginButton;
