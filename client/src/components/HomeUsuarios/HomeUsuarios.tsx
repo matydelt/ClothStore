5860bebd8c1cd1adb78fbd0036e41fa0fa0b4534
ZuperPatito <chony1208@gmail.com>
2021-11-29 11:31:56 -0300

    aca estamos

--- client/src/components/HomeUsuarios/HomeUsuarios.tsx
+++ client/src/components/HomeUsuarios/HomeUsuarios.tsx
@@ -1,165 +1,207 @@
 import * as React from 'react'
-import { Avatar,Box} from '@material-ui/core'
+import { Avatar, Box } from '@material-ui/core'
 import "./HomeUsuarios.css"
-import { TextField } from '@mui/material'
+import { Badge, Card, TextField } from '@mui/material'
 import axios from 'axios'
 import { useEffect } from 'react'
-import CardPublicacion from '../home/publicaciones/cardPublicaciones/cardPublicaciones'
+import CardPublicacion from '../HomePage/publicaciones/cardPublicaciones/cardPublicaciones'
+import { useSelector } from 'react-redux'
+import { RootState } from '../../redux/store/store'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 
 interface FormUserInterface {
-    photo:string;
+    photo: string;
     phone: string;
     email: string;
-    name:{firstName: string,
-        lastName: string};
+    name: {
+        firstName: string,
+        lastName: string
+    };
     dni: string;
     calle: string;
     numero: string;
     ciudad: string;
     cp: string;
     publications: any[];
-    shopping:any[];
-  }
-  interface Props{
-    name: string;
-    images: any[];
-    stock: number;
-    mark: string;
-    detail: string;
-    price: number;
-    categorie: string;
-    author: string;
-    gender: string;
-    key:string;
+    shopping: any[];
 }
+// interface Props {
+//     name: string;
+//     images: any[];
+//     stock: number;
+//     mark: string;
+//     detail: string;
+//     price: number;
+//     categorie: string;
+//     author: string;
+//     gender: string;
+//     key: string;
+// }
 export default function HomeUsuarios() {
-    
-    
+    const user = useSelector((state: RootState) => state.userSignin.userInfo)
     const [input, setInput] = React.useState<FormUserInterface>({
-        photo:"",
+        photo: "",
         phone: "",
         email: "",
-        name:{firstName: "",
-        lastName: "",},
+        name: {
+            firstName: "",
+            lastName: "",
+        },
         dni: "",
-        calle:"",
-        numero:"",
-        ciudad:"",
-        cp:"",
-        publications:[],
-        shopping:[]
-        });
-useEffect(() => {
-    async function getOneUser(){
-        await axios.get("http://localhost:3001/auth/619e9d1adcffb9b31ee1fca8").then(({data})=>setInput(data))
+        calle: "",
+        numero: "",
+        ciudad: "",
+        cp: "",
+        publications: [],
+        shopping: []
+    });
+    useEffect(() => {
+        async function getOneUser() {
+            await axios.get(`http://localhost:3001/auth/${user?._id}`).then(({ data }) => setInput({ ...input, ...data }))
+        }
+        getOneUser()
+    }, [])
+
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        setInput({ ...input, [e.target.name]: e.target.value });
+    };
+    function handleClickEdit(e: React.SyntheticEvent) {
+        e.preventDefault()
+        console.log(input)
     }
-    getOneUser()
-}, [])
-
-
-const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    setInput({ ...input, [e.target.name]: e.target.value });
-  };
-function handleClickEdit(e:React.SyntheticEvent){
-    e.preventDefault() 
-    console.log(input)
-}
-
     return (
-        <Box component="form" className="form-home-usuario">
+        <Box>
+            <Box component="form" className="form-home-usuario">
 
-        <label>Mis Datos</label>
-            <Avatar alt={input.photo ? input.photo : input.name.firstName[0]} className="avatar-usuario">{input.name.firstName[0]} </Avatar>
+                <label>Mis Datos</label>
+                <Avatar alt={input?.photo ? input.photo : input?.name.firstName[0]} className="avatar-usuario">{input.name.firstName[0]} </Avatar>
 
-        <legend>Datos de la Cuenta</legend>
-        <div className="div-field">
-            <TextField
-            disabled={true}
-            label="Teléfono:"
-            value={input.phone}
-            onChange={handleChange}
-            />
-            <TextField
-            disabled
-            label="E-mail:"
-            value={input.email}
-            onChange={handleChange}
-            />
-        </div>
-        
+                <legend>Datos de la Cuenta</legend>
+                <div className="div-field">
+                    <TextField
+                        disabled
+                        label="Teléfono:"
+                        value={input.phone}
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        disabled
+                        label="E-mail:"
+                        value={input.email}
+                        onChange={handleChange}
+                    />
+                </div>
 
-        <legend>Datos personales</legend>
-        <div className="div-field">
-            <TextField
-            disabled
-            label="Nombre:"
-            value={input.name.firstName}
-            onChange={handleChange}
-            />
-        
-            <TextField
-            disabled
-            label="Apellido:"
-            value={input.name.lastName}
-            onChange={handleChange}
-            />
-        </div>
-        <div className="div-field">
-            <TextField
-            disabled
-            label="DNI:"
-            value={input.dni}
-            onChange={handleChange}
-            />
-        </div>
 
-        <legend>Domicilio</legend>
-        <div className="div-field">
-            <TextField
-            disabled
-            label="Calle:"
-            value={input.calle}
-            onChange={handleChange}
-            />
-        
-            <TextField
-            disabled
-            label="Número:"
-            value={input.numero}
-            onChange={handleChange}
-            />
-        </div>
-        <div className="div-field">
-            <TextField
-            disabled
-            label="Ciudad:"
-            value={input.ciudad}
-            onChange={handleChange}
-            />
-        
-            <TextField
-            disabled
-            label="Código postal:"
-            value={input.cp}
-            onChange={handleChange}
-            />
-        </div>
-        <div>
-           <button onClick={handleClickEdit}>guardar</button>
-           <button onClick={handleClickEdit}>editar</button>
-        </div>
-        <div>
-            <h3>Publicaciones</h3>
-        {input.publications.length > 0 ? 
-        input.publications.map((e)=>{return(<CardPublicacion  name={e.name} author={e.author} images={e.images} mark={e.mark} stock={e.stock} price={e.price} categorie={e.categorie} detail={e.detail} gender={e.gender} key={e._id}/>)}) : 
-        <h4>No se han realizado publicaciones</h4>}
-        </div>
-        <div>
-            <h3>Compras</h3>
-        {input.shopping.length > 0 ? 
-        input.shopping.map((e)=>{return(<CardPublicacion  name={e.name} author={e.author} images={e.images} mark={e.mark} stock={e.stock} price={e.price} categorie={e.categorie} detail={e.detail} gender={e.gender} key={e._id}/>)}) : 
-        <h4>No se han realizado Compras</h4>}
-        </div>
-    </Box>)
+                <legend>Datos personales</legend>
+                <div className="div-field">
+                    <TextField
+                        disabled
+                        label="Nombre:"
+                        value={input.name.firstName}
+                        onChange={handleChange}
+                    />
+
+                    <TextField
+                        disabled={true}
+                        label="Apellido:"
+                        value={input.name.lastName}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="div-field">
+                    <TextField
+                        disabled
+                        label="DNI:"
+                        value={input.dni}
+                        onChange={handleChange}
+                    />
+                </div>
+
+                <legend>Domicilio</legend>
+                <div className="div-field">
+                    <TextField
+                        disabled
+                        label="Calle:"
+                        value={input.calle}
+                        onChange={handleChange}
+                    />
+
+                    <TextField
+                        disabled
+                        label="Número:"
+                        value={input.numero}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="div-field">
+                    <TextField
+                        disabled
+                        label="Ciudad:"
+                        value={input.ciudad}
+                        onChange={handleChange}
+                    />
+
+                    <TextField
+                        disabled
+                        label="Código postal:"
+                        value={input.cp}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div>
+                    <button onClick={handleClickEdit}>guardar</button>
+                    <button onClick={handleClickEdit}>editar</button>
+                </div>
+            </Box>
+            <div>
+                <h3>Publicaciones</h3>
+                {input.publications.length > 0 ?
+                    <TableContainer component={Paper}>
+                        <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+                            <TableHead>
+                            <TableRow>
+                                <TableCell>Nombre de articulo</TableCell>
+                                <TableCell align="right">Marca</TableCell>
+                                <TableCell align="right">Categoria</TableCell>
+                                <TableCell align="right">Genero</TableCell>
+                                <TableCell align="right">Precio</TableCell>
+                            </TableRow>
+                            </TableHead>
+                            <TableBody>
+                        {input.publications.map((e) =>(
+                            <TableRow
+                            key={e.name}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                          >
+                            <TableCell component="th" scope="row">
+                              {e.name}
+                            </TableCell>
+                            <TableCell align="right">{e.mark}</TableCell>
+                            <TableCell align="right">{e.categorie}</TableCell>
+                            <TableCell align="right">{e.gender}</TableCell>
+                            <TableCell align="right">{e.price}</TableCell>
+                          </TableRow>
+                        ))}
+                            </TableBody>
+                            </Table>
+                    </TableContainer>
+                        :
+                    <h4>No se han realizado publicaciones</h4>}
+            </div>
+            <div>
+                <h3>Compras</h3>
+                {input.shopping.length > 0 ?
+                    input.shopping.map((e) => { return (<CardPublicacion id={e._id} name={e.name} author={e.author} images={e.images} mark={e.mark} stock={e.stock} price={e.price} categorie={e.categorie} detail={e.detail} gender={e.gender} key={e._id} />) }) :
+                    <h4>No se han realizado Compras</h4>}
+            </div>
+        </Box>)
 }
