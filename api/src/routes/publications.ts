e6099b5bd2b3aff8c86bc5f929db72b24283e068
lautaromugas8 <lautaro_mugas@hotmail.com>
2021-12-02 11:35:54 -0300

    put

--- api/src/routes/publications.ts
+++ api/src/routes/publications.ts
@@ -2,9 +2,10 @@ import express from "express";
 import PublicationController from "../controllers/publicationController";
 const router = express.Router();
 
-router.get("/publications", PublicationController.putPublications);
+router.put("/publications", PublicationController.putPublications);
 router.get("/publication", PublicationController.getPublication);
 router.post("/publications/new", PublicationController.setPublication);
 router.delete("/publications/:_id", PublicationController.deletePublications);
 router.put("/publications/stock", PublicationController.putStock);
+
 module.exports = router;
