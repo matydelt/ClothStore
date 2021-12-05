5036a68deae5eaa1616b750aa1d4212b77b40069
matydelt <matymanda@hotmail.com>
2021-12-05 20:24:00 -0300

    mensajes en admin page  y employee page agregados

--- client/src/components/adminPage/employeePage.tsx
+++ client/src/components/adminPage/employeePage.tsx
@@ -18,7 +18,9 @@ function EmployeePage() {
                 <Box sx={{ borderRadius: "10px", backgroundColor: "#3562", width: "50%", height: "50%" }}>
                     <p style={{ marginLeft: "5px" }}>Bienvenido! esta es la seccion de los empleados y admin!.</p>
                     <br></br>
-                    <p style={{ marginLeft: "5px" }}>Publicaciones : Esta seccion sera donde podras ver las</p>
+                    <p style={{ marginLeft: "5px" }}>Publicaciones : Esta seccion sera donde podras ver las publicaciones que actualmente estan en revision , ahi podras aceptarlas o rechazarlas</p>
+                    <br></br>
+                    <p>Denuncias: Aqui podras ver las publicaciones denunciadas por los usuarios , tal vez a algun empleado se le paso algo por alto al momento de aceptar una publicacion o tal vez haya algun problema con el vendedor</p>
 
                 </Box>
             </Box>
