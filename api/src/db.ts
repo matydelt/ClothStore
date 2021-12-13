77f68011693bf1afcf2a31b0c8337431e1cae079
keilo-bravo <kesbto@gmail.com>
2021-12-13 15:04:53 -0300

    HOST DB test

--- api/src/db.ts
+++ api/src/db.ts
@@ -2,5 +2,5 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config()
-const conn = mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`)
+const conn = mongoose.connect(`${process.env.DB_ENLACE}`)
 export default conn
