ff58177518c411260f405de2537f8f703d7ee8eb
Christian Nordfors <christiannordfors@gmail.com>
2021-12-13 14:26:34 -0300

    cambio publicaciones relacionadas

--- client/src/components/publicationDetail/relatedPublications/RelatedPublications.tsx
+++ client/src/components/publicationDetail/relatedPublications/RelatedPublications.tsx
@@ -37,7 +37,7 @@ export default function RelatedPublications({ publicationId }: props): JSX.Eleme
             console.log(data)
             setPublications(data);
         });
-    }, []);
+    }, [publicationId]);
 
 
     return (
