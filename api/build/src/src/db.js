1bf210df7a6c6a4c612e424161724b1453ed70f8
matydelt <matymanda@hotmail.com>
2021-12-13 15:11:38 -0300

    server mongo agregado a la api

--- api/build/src/src/db.js
+++ api/build/src/src/db.js
@@ -6,5 +6,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-const conn = mongoose_1.default.connect(`mongodb://matydelt:matias133@clothstore.jqw1q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`);
+const conn = mongoose_1.default.connect(`mongodb+srv://matydelt:matias133@clothstore.jqw1q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`);
 exports.default = conn;
