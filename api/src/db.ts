b9242104cc18c4ebb0d69d3ee4a28be3bcd6e2e6
ajlveloper <ajveloper@gmail.com>
2021-12-13 20:53:42 -0430

    mal con la mirror

--- api/src/db.ts
+++ api/src/db.ts
@@ -2,5 +2,5 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config()
-const conn = mongoose.connect(`mongodb+srv://matydelt:matias133@clothstore.jqw1q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
+const conn = mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`)
 export default conn
