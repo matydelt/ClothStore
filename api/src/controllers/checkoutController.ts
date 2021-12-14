fff8115d2b773bd9938e73f97200f9f572a7a6ad
keilo-bravo <kesbto@gmail.com>
2021-12-14 19:18:18 -0300

    ruta deploy

--- api/src/controllers/checkoutController.ts
+++ api/src/controllers/checkoutController.ts
@@ -50,7 +50,7 @@ export default class Checkout {
                 additional_info: order._id,
                 statement_descriptor: "Clothstore",
                 back_urls: { failure: '', pending: '', success: 'http://localhost:3000/' },
-                notification_url: "https://hookb.in/Px0gpPOPE7hpKPrrKWP9",
+                notification_url: "https://cloth-store-henry.herokuapp.com/statusmp",
             }
             const response = await mercadoPago.preferences.create(preference)
             
