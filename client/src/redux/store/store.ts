258082e653984f57168360c3ed3ba5b1f5b66fa3
lautaromugas8 <lautaro_mugas@hotmail.com>
2022-02-13 15:44:51 -0300

    Actualicé el redux dev tools

--- client/src/redux/store/store.ts
+++ client/src/redux/store/store.ts
@@ -1,11 +1,11 @@
-import { createStore, applyMiddleware } from 'redux'
-import thunkMiddleware from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import rootReducer from '../reducer'
+import { createStore, applyMiddleware } from "redux";
+import thunkMiddleware from "redux-thunk";
+import { composeWithDevTools } from "@redux-devtools/extension";
+import rootReducer from "../reducer";
 
-const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware))
+const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware));
 
 // The store now has the ability to accept thunk functions in `dispatch`
-const store = createStore(rootReducer, composedEnhancer)
-export default store
-export type RootState = ReturnType<typeof rootReducer>
+const store = createStore(rootReducer, composedEnhancer);
+export default store;
+export type RootState = ReturnType<typeof rootReducer>;
