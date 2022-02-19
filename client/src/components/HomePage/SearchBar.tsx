267ee86ab75ff091e9ea9e8071851fe7b3f4b0ad
lautaromugas8 <lautaro_mugas@hotmail.com>
2022-02-18 21:24:00 -0300

    Ahora esta centrada

--- client/src/components/HomePage/SearchBar.tsx
+++ client/src/components/HomePage/SearchBar.tsx
@@ -2,21 +2,23 @@ import * as React from "react";
 import { useDispatch } from "react-redux";
 import { putPublications } from "../../redux/actions/publicationActions";
 import Box from "@mui/material/Box";
-import { TextField } from "@material-ui/core";
-import InputAdornment from '@mui/material/InputAdornment';
+import { TextField } from "@mui/material";
+import InputAdornment from "@mui/material/InputAdornment";
 import SearchIcon from "@mui/icons-material/Search";
-import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import { Theme } from "@mui/material/styles";
+import createStyles from "@mui/styles/createStyles";
+import makeStyles from "@mui/styles/makeStyles";
 import Autocomplete from "./Autocomplete";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     textField: {
-      width: '74%'
-    }
+      width: "84%",
+      paddingLeft: "1rem",
+      paddingTop: "1rem",
+    },
   })
 );
-
-
 
 function SearchBar() {
   const [text, setText] = React.useState("");
@@ -26,16 +28,17 @@ function SearchBar() {
 
   const handleSearchBar = (event: any): void => {
     dispatch(putPublications({ name: text }));
-    setText('');
-  }
+    setText("");
+  };
 
-  const handleSearchBarEnter: React.KeyboardEventHandler<HTMLDivElement> = (event): void => {
+  const handleSearchBarEnter: React.KeyboardEventHandler<HTMLDivElement> = (
+    event
+  ): void => {
     if (event.key === "Enter") {
       dispatch(putPublications({ name: text }));
-      setText('');
+      setText("");
     }
-  }
-
+  };
 
   return (
     <Box>
